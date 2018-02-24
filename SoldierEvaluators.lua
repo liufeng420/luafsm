12ef4f231b378342b1d20a09ac4be32d866d0ee3
linjs <linjs@woobest.com>
2018-02-24 14:42:18 +0800

    remove

--- SoldierEvaluators.lua
+++
@@ -1,9 +0,0 @@
-function SoldierEvaluators_True(userData)
-    print("SoldierEvaluators_True data is "..userData)
-    return true
-end
-
-function SoldierEvaluators_False(userData)
-    print("SoldierEvaluators_True data is "..userData)
-    return false
-end
